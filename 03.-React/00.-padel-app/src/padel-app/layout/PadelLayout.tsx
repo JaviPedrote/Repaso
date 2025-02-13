5edfec30a1ce998859e654ea751c99ff662207ec
JaviPedrote <pedrote3.1416gmail.com>
2025-02-13 12:21:45 +0100

    padelAppkeyFrames

--- 03.-React/00.-padel-app/src/padel-app/layout/PadelLayout.tsx
+++ 03.-React/00.-padel-app/src/padel-app/layout/PadelLayout.tsx
@@ -6,6 +6,7 @@ export const PadelLayout = () => {
     <div className="relative min-h-[calc(100vh-50px)] flex flex-col items-center justify-center">
     <div className="absolute inset-0 before:absolute before:inset-0 before:bg-[url('/Padel_Blog.jpg')] before:bg-cover before:opacity-60 before:content-['']"></div>
     <div className="relative z-10 w-full">
+    
       <NavBar />
       <Outlet />
     </div>
--- 03.-React/00.-padel-app/src/padel-app/views/MatchesViews.tsx
+++ 03.-React/00.-padel-app/src/padel-app/views/MatchesViews.tsx
@@ -2,6 +2,8 @@
 
 export const MatchesViews = () => {
   return (
-    <div className="h-screen">MatchesViews</div>
+    <div className="h-screen">
+      <div id="miElemento" className=" sticky top-10 left-0 bg-red-900 h-10 "></div>
+      </div>
   )
 }
